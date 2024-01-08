232741df9836253fbd32fc6f5a2969d131f0f41e
Stixxy <100129571+StixxyTape@users.noreply.github.com>
2024-01-08 21:17:11 +0000

    Add files via upload

--- Connection Maker 2/Scripts/Car.gd
+++ Connection Maker 2/Scripts/Car.gd
@@ -1,10 +1,8 @@
 extends CharacterBody2D
 
 # Speed
-var speed : float = 200.0
-
-# The destination
-var targetPos : Vector2 = Vector2(358.0, 73.0)
+var speed : float = 20.0
+var spawnPos : Vector2 = position
 
 @onready var navAgent : NavigationAgent2D = $NavigationAgent2D
 
@@ -17,9 +15,9 @@ func actor_setup():
 	
 	# Wait for the first physics frame so the NavigationServer can sync.
 	await get_tree().physics_frame
-
+	await get_tree().create_timer(1).timeout
 	# Now that the navigation map is no longer empty, set the movement target.
-	set_movement_target(targetPos)
+	set_movement_target(Global.cinemaPos)
 
 func set_movement_target(movement_target: Vector2):
 	navAgent.target_position = movement_target
@@ -28,7 +26,9 @@ func _physics_process(delta):
 	# If reached destination, don't run the rest of this code
 	if navAgent.is_navigation_finished():
 		return
-
+	elif (!navAgent.is_target_reachable() and position != spawnPos):
+		position = spawnPos
+		
 	var currentAgentPos: Vector2 = global_position
 	var nextPathPos: Vector2 = navAgent.get_next_path_position()
 
