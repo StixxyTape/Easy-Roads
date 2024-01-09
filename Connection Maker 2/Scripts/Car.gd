bbf3526c8da072a0a0559593c8f852aa9c0a57b8
Bram <145325665+Bram-VE@users.noreply.github.com>
2024-01-09 20:21:07 +0100

    Updating the car

--- Connection Maker 2/Scripts/Car.gd
+++ Connection Maker 2/Scripts/Car.gd
@@ -3,12 +3,15 @@ extends CharacterBody2D
 # Speed
 var speed : float = 20.0
 var spawnPos : Vector2
+var curScale = get_scale()
 
+@onready var sprite : Sprite2D = $Icon
 @onready var navAgent : NavigationAgent2D = $NavigationAgent2D
 
 func _ready():
 	# Make sure to not await during _ready.
 	call_deferred("actor_setup")
+	rotation = 0
 
 func actor_setup():
 	# Wait for the first physics frame so the NavigationServer can sync.
@@ -23,6 +26,15 @@ func set_movement_target(movement_target: Vector2):
 func _physics_process(_delta):
 	var currentAgentPos: Vector2 = global_position
 	var nextPathPos: Vector2 = navAgent.get_next_path_position()
+	
+	if nextPathPos.x > position.x:
+		set_scale(Vector2(curScale.x,curScale.y))
+		sprite.offset.y = 1
+	else:
+		sprite.offset.y = -5
+		set_scale(Vector2(curScale.x,-curScale.y))
+	
+	look_at(nextPathPos)
 	
 	# If reached destination, don't run the rest of this code
 	if navAgent.is_navigation_finished():
