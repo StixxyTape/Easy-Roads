a4f2393f995a8ac841ae865c347f71c8a872154f
Stixxy <100129571+StixxyTape@users.noreply.github.com>
2024-01-13 22:57:34 +0000

    Delete Connection Maker 2 directory

--- Connection Maker 2/Scripts/GameOver.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var descriptionLabel : Label = $CanvasLayer/Description
-
-func _ready():
-	if Global.day == 1:
-		descriptionLabel.text = str("You made it to ", Global.day, " Day")
-	else:
-		descriptionLabel.text = str("You made it to ", Global.day, " Days")
